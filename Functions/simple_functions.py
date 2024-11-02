04e8afb7e761faa9f5967fd41ce8712c130750ed
Kaycaitt <caitlantbeard@gmail.com>
2024-11-02 11:39:09 -0500

    Uploading completed simple_functions.py exercise.

---
+++ Functions/simple_functions.py
@@ -0,0 +1,16 @@
+def favorite_things(name, movie, band):
+    return("My name is "+ name + "\nMy favorite movie is " + movie + "\nMy favorite band is " + band)
+    
+print(favorite_things("Caitlan", "X-Men: First Class", "The Pretty Reckless"))
+
+
+def why_im_here():
+    return("\nI joined the Data Analytics program to get further in life and to help support my loved ones.")
+
+print(why_im_here())
+
+def favorite_place(place, why):
+    return("\nMy favorite place to visit is " + place + " and I would like to visit because I love the aesthetic and culture. I also want to visit " + why + ".")
+
+print(favorite_place("Japan", "Cat Island"))
+print(favorite_place("United Kingdom", "Kyoto Botanical Garden"))
